c28a58f8cc5ffed6abc4d4caced0aad93c3e1646
Oleg Lyamzin <o.s.like@yandex.ru>
2021-02-05 16:48:03 +0300

    Dencrypt and Enumerables

--- Crypto.Vigenere/Vigenere.cs
+++ Crypto.Vigenere/Vigenere.cs
@@ -1,8 +1,71 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
-namespace Crypto.Vigenere
+namespace Crypto
 {
-    public class Class1
+    public class Vigenere
     {
+        private char[] _alphabet = new char[] {'A', 'B','C','D', 'E',
+                                               'F','G','H','I','J',
+                                               'K','L','M','N','O',
+                                               'P','Q','R','S','T',
+                                               'U','V','W','X','Y','Z'};
+        private int N = 26;
+        public string Encrypt(string source, string key)
+        {
+            if(key == "")
+            {
+                return source.ToUpper();
+            }
+            source = source.ToUpper();
+            key = key.ToUpper();
+            int iKey = 0;
+            StringBuilder sb = new StringBuilder();
+            foreach (char symbol in source)
+            {
+                int indexSymb = Array.IndexOf(_alphabet, symbol);
+                int difference = Array.IndexOf(_alphabet, key[iKey % key.Length]);
+                sb.Append(_alphabet[(indexSymb + difference) % N]);
+                iKey++;
+            }
+            return sb.ToString();
+        }
+
+        public string Dencrypt(string encrypted, string key)
+        {
+            if (key == "")
+            {
+                return encrypted.ToUpper();
+            }
+            encrypted = encrypted.ToUpper();
+            key = key.ToUpper();
+            int iKey = 0;
+            StringBuilder sb = new StringBuilder();
+            foreach (char symbol in encrypted)
+            {
+                int indexSymb = Array.IndexOf(_alphabet, symbol);
+                int difference = Array.IndexOf(_alphabet, key[iKey % key.Length]);
+                sb.Append(_alphabet[Math.Abs(indexSymb + N - difference) % N]);
+                iKey++;
+            }
+            return sb.ToString();
+        }
+
+        public IEnumerable<string> Encrypt (IEnumerable<string> source, string key)
+        {
+            foreach(string scr in source)
+            {
+                yield return Encrypt(scr, key);
+            }
+        }
+
+        public IEnumerable<string> Dencrypt(IEnumerable<string> encrypted, string key)
+        {
+            foreach (string scr in encrypted)
+            {
+                yield return Dencrypt(scr, key);
+            }
+        }
     }
 }
